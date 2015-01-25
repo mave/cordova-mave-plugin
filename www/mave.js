90260bca0071f3cf3320935527e07335f72f4059
Greg Einfrank <greg.einfrank@gmail.com>
2015-01-25 11:19:15 -0500

    Add cordova.exec in mave.js

--- www/mave.js
+++ www/mave.js
@@ -1,7 +1,13 @@
 var maveExport = {};
 
 maveExport.init = function (applicationId) {
-
+  var successCallback = function () {
+      console.log('success');
+  };
+  var errorCallback = function () {
+      console.log('error');
+  };
+  cordova.exec(successCallback, errorCallback, "Mave", "init", [applicationId]);
 };
 
 module.exports = maveExport;
