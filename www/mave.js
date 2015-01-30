f5fb44230d6911be0ee1157f78bd59f8a02e3453
Greg Einfrank <greg.einfrank@gmail.com>
2015-01-30 17:35:07 -0500

    Add callbacks to all functions, and do some error checking

--- www/mave.js
+++ www/mave.js
@@ -1,136 +1,132 @@
 var maveExport = {};
 
-maveExport.setupSharedInstanceWithApplicationID = function (applicationId) {
-  var successCallback = function () {
+maveExport.setupSharedInstanceWithApplicationID = function (applicationId, successCallback, errorCallback) {
+  successCallback = successCallback || function () {
       console.log('init success');
   };
-  var errorCallback = function () {
-      console.log('init error');
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('init error: ' + errorMessage);
   };
-  cordova.exec(successCallback, errorCallback, "Mave", "setupSharedInstanceWithApplicationID", [applicationId]);
+  if ( applicationId === undefined ) {
+    errorCallback('No Application ID was passed in.');
+  } else {
+    cordova.exec(successCallback, errorCallback, "Mave", "setupSharedInstanceWithApplicationID", [applicationId]);
+  }
 };
 
-maveExport.identifyUser = function (userData) {
-  var successCallback = function () {
+maveExport.identifyUser = function (userData, successCallback, errorCallback) {
+  successCallback = successCallback || function () {
       console.log('identifyUser success');
   };
-  var errorCallback = function () {
-      console.log('identifyUser error');
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('identifyUser error: ' + errorMessage);
   };
   var userID = userData.userID;
   var firstName = userData.firstName;
   var lastName = userData.lastName;
   var email = userData.email;
   var phone = userData.phone;
-  cordova.exec(successCallback, errorCallback, "Mave", "identifyUser", [userID, firstName, lastName, email, phone]);
+  if ( userID === undefined ) {
+    errorCallback('userID is required');
+  } else if ( firstName === undefined ) {
+    errorCallback('firstName is required');
+  } else {
+    cordova.exec(successCallback, errorCallback, "Mave", "identifyUser", [userID, firstName, lastName, email, phone]);
+  }
 };
 
-maveExport.getReferringUser = function (successCallback) {
-  var errorCallback = function ( data ) {
-      console.log('getReferringUser error');
+maveExport.getReferringUser = function (successCallback, errorCallback) {
+  successCallback = successCallback || function () {
+      console.log('getReferringUser success');
+  };
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('getReferringUser error: ' + errorMessage);
   };
   cordova.exec(successCallback, errorCallback, "Mave", "getReferringUser", []);
 };
 
-maveExport.identifyAnonymousUser = function () {
-  var successCallback = function () {
+maveExport.identifyAnonymousUser = function (successCallback, errorCallback) {
+  successCallback = successCallback || function () {
       console.log('identifyAnonymousUser success');
   };
-  var errorCallback = function () {
-      console.log('identifyAnonymousUser error');
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('identifyAnonymousUser error: ' + errorMessage);
   };
   cordova.exec(successCallback, errorCallback, "Mave", "identifyAnonymousUser", []);
 };
 
-maveExport.presentInvitePageModally = function (successCallback) {
-  if ( successCallback === undefined ) {
-    successCallback = function () {
-      console.log('presentInvitePageModally success');
-    };
-  }
-  var errorCallback = function () {
-      console.log('presentInvitePageModally error');
+maveExport.presentInvitePageModally = function (successCallback, errorCallback) {
+  successCallback = successCallback || function () {
+    console.log('presentInvitePageModally success');
+  };
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('presentInvitePageModally error: ' + errorMessage);
   };
   cordova.exec(successCallback, errorCallback, "Mave", "presentInvitePageModallyWithBlock", []);
 };
 
-maveExport.trackSignup = function () {
-  var successCallback = function () {
+maveExport.trackSignup = function (successCallback, errorMessage) {
+  successCallback = successCallback || function () {
       console.log('trackSignup success');
   };
-  var errorCallback = function () {
-      console.log('trackSignup error');
+  errorCallback = errorCallback || function (errorMessage) {
+      console.log('trackSignup error: ' + errorMessage);
   };
   cordova.exec(successCallback, errorCallback, "Mave", "trackSignup", []);
 };
 
 
 /* Configuration */
-maveExport.setBackButtonTitle = function (title) {
-  var successCallback = function () {
-      console.log('setBackButtonTitle success');
-  };
-  var errorCallback = function () {
-      console.log('setBackButtonTitle error');
-  };
-  cordova.exec(successCallback, errorCallback, "Mave", "setBackButtonTitle", [title]);
-};
-
-maveExport.setBackButtonTintColor = function (color) {
-  var successCallback = function () {
-      console.log('setBackButtonTintColor success');
-  };
-  var errorCallback = function () {
-      console.log('setBackButtonTintColor error');
-  };
-  cordova.exec(successCallback, errorCallback, "Mave", "setBackButtonTintColor", [color]);
-};
-
-maveExport.setDisplayOptions = function (displayOptions) {
+maveExport.setDisplayOptions = function (displayOptions, successCallback, errorCallback) {
   displayOptions = displayOptions || {};
-  var successCallback = function () {
-      console.log('setDisplayOptions success');
+  successCallback = successCallback || function () {
+    console.log('setDisplayOptions success');
   };
-  var errorCallback = function () {
-      console.log('setDisplayOptions error');
+  errorCallback = errorCallback || function (errorMessage) {
+    console.log('setDisplayOptions error: ' + errorMessage);
   };
-  cordova.exec(successCallback, errorCallback, "Mave", "setNavigationBarOptions", [
+  cordova.exec(function () {
+    cordova.exec(function () {
+      cordova.exec(function () {
+        cordova.exec(function () {
+          cordova.exec(function () {
+            cordova.exec(successCallback, errorCallback, "Mave", "setSharePageOptions", [
+              displayOptions.sharePageBackgroundColor,
+              displayOptions.sharePageIconColor,
+              displayOptions.sharePageIconTextColor,
+              displayOptions.sharePageExplanationTextColor
+            ]);
+          }, errorCallback, "Mave", "setMessageSectionOptions", [
+            displayOptions.messageFieldTextColor,
+            displayOptions.messageFieldBackgroundColor,
+            displayOptions.sendButtonCopy,
+            displayOptions.sendButtonTextColor,
+            displayOptions.bottomViewBorderColor,
+            displayOptions.bottomViewBackgroundColor
+          ]);
+        }, errorCallback, "Mave", "setContactSectionOptions", [
+          displayOptions.contactSectionHeaderTextColor,
+          displayOptions.contactSectionHeaderBackgroundColor,
+          displayOptions.contactSectionIndexColor,
+          displayOptions.contactSectionIndexBackgroundColor
+        ]);
+      }, errorCallback, "Mave", "setContactOptions", [
+        displayOptions.contactNameTextColor,
+        displayOptions.contactDetailsTextColor,
+        displayOptions.contactSeparatorColor,
+        displayOptions.contactCellBackgroundColor,
+        displayOptions.contactCheckmarkColor
+      ]);
+    }, errorCallback, "Mave", "setInviteExplanationOptions", [
+      displayOptions.inviteExplanationTextColor,
+      displayOptions.inviteExplanationCellBackgroundColor
+    ]);
+  }, errorCallback, "Mave", "setNavigationBarOptions", [
     displayOptions.navigationBarTitleCopy,
     displayOptions.navigationBarTitleTextColor,
     displayOptions.navigationBarBackgroundColor,
     displayOptions.navigationBarCancelButtonTitle,
     displayOptions.navigationBarCancelButtonTintColor
-  ]);
-  cordova.exec(successCallback, errorCallback, "Mave", "setInviteExplanationOptions", [
-    displayOptions.inviteExplanationTextColor,
-    displayOptions.inviteExplanationCellBackgroundColor
-  ]);
-  cordova.exec(successCallback, errorCallback, "Mave", "setContactOptions", [
-    displayOptions.contactNameTextColor,
-    displayOptions.contactDetailsTextColor,
-    displayOptions.contactSeparatorColor,
-    displayOptions.contactCellBackgroundColor,
-    displayOptions.contactCheckmarkColor
-  ]);
-  cordova.exec(successCallback, errorCallback, "Mave", "setContactSectionOptions", [
-    displayOptions.contactSectionHeaderTextColor,
-    displayOptions.contactSectionHeaderBackgroundColor,
-    displayOptions.contactSectionIndexColor,
-    displayOptions.contactSectionIndexBackgroundColor
-  ]);
-  cordova.exec(successCallback, errorCallback, "Mave", "setMessageSectionOptions", [
-    displayOptions.messageFieldTextColor,
-    displayOptions.messageFieldBackgroundColor,
-    displayOptions.sendButtonCopy,
-    displayOptions.sendButtonTextColor,
-    displayOptions.bottomViewBorderColor,
-    displayOptions.bottomViewBackgroundColor
-  ]);
-  cordova.exec(successCallback, errorCallback, "Mave", "setSharePageOptions", [
-    displayOptions.sharePageBackgroundColor,
-    displayOptions.sharePageIconColor,
-    displayOptions.sharePageIconTextColor,
-    displayOptions.sharePageExplanationTextColor
   ]);
 };
 
